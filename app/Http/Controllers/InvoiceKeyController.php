a51881dd66d5b538efe3e1b7e2fa3a506192ff50
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-09-09 13:53:09 +0100

    Feat:Add store,filter,index and routes for invoiceKeys

---
+++ app/Http/Controllers/InvoiceKeyController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\InvoiceKey;
+use App\Http\Resources\InvoiceKeyCollection;
+use Illuminate\Support\Facades\Validator;
+
+use Illuminate\Http\Request;
+
+class InvoiceKeyController extends Controller
+{
+    public function index(){
+        $invoiceKeys = InvoiceKey::all();
+        return new InvoiceKeyCollection($invoiceKeys);
+    }
+
+    public function store(Request $request){
+        $validatedData=Validator::make($request->all(),[
+            'Invoicekey' => 'required|string|unique:TInvoicekeys,Invoicekey',
+            'DirectoryFId'=>'required|int'
+        ]);
+
+        if($validatedData->fails()){
+            return response()->json([
+                'status'=>401,
+                'message'=> "Echec d'enregistrement",
+                "errors"=>$validatedData->errors(),
+            ]);
+        }
+        $directory = InvoiceKey::create($request->all());
+        return response()->json([
+            'status'=>201,
+            'message'=> "Enregistrement reussie",
+        ]);
+    }
+
+
+}
